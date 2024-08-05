321a6fa73a1b8c15505e5be3e3e5ce6ce04f1260
luunhungoc <ngoc.luunhu@gmail.com>
2024-08-05 20:49:20 +0700

    update day02_Spring Config

--- src/main/java/javabasedconfiguration/BeanConfiguration.java
+++ src/main/java/javabasedconfiguration/BeanConfiguration.java
@@ -1,25 +1,25 @@
-//package javabasedconfiguration;
-//
-//import org.springframework.context.annotation.Bean;
-//import pojobeans.AccountRepository;
-//import pojobeans.AccountRepositoryImpl;
-//import pojobeans.AccountService;
-//import pojobeans.AccountServiceImpl;
-//
-//import javax.management.MXBean;
-//
-//public class BeanConfiguration {
-//    @Bean
-//    public AccountService accountService(){
-//        AccountServiceImpl bean = new AccountServiceImpl();
-//        bean.setAccountRepository(accountDAO());
-//        return bean;
-//    }
-//
-//    @Bean
-//    public AccountRepository accountDAO(){
-//        AccountRepositoryImpl bean = new AccountRepositoryImpl();
-//        return bean;
-//    }
-//
-//}
+package javabasedconfiguration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import pojobeans.AccountRepository;
+import pojobeans.AccountRepositoryImpl;
+import pojobeans.AccountService;
+import pojobeans.AccountServiceImpl;
+
+@Configuration
+public class BeanConfiguration {
+    @Bean
+    public AccountService accountService(){
+        AccountServiceImpl bean = new AccountServiceImpl();
+        bean.setAccountRepository(accountDAO());
+        return bean;
+    }
+
+    @Bean
+    public AccountRepository accountDAO(){
+        AccountRepositoryImpl bean = new AccountRepositoryImpl();
+        return bean;
+    }
+
+}
